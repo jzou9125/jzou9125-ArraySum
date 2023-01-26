a30ae4de44ea4637984caee209ba92cedc83d0c0
Jason Zou <jasonzou@buffalo.edu>
2023-01-26 02:54:57 +0000

    completed

--- src/main/java/ArraySum.java
+++ src/main/java/ArraySum.java
@@ -1,3 +1,5 @@
+import java.util.List;
+
 public class ArraySum {
     /**
      * This method should return the sum of all ints in an array - this is a very useful trick to know!
@@ -7,7 +9,10 @@ public class ArraySum {
      * @return the sum of all values in arr.
      */
     public int sum(int[] arr){
-
-        return 0;
+        int sum = 0;
+        for (int num: arr){
+            sum += num;
+        }
+        return sum;
     }
 }
